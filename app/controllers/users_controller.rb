b8f78973c5d9f956cb164294aa8b73a485e732b5
tjurrema <tessa_jurrema@hotmail.com>
2013-11-16 19:08:48 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -8,8 +16,33 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  #to delete users
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+  
+  #editing your profile page
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  #to update the users profile
+  
+    def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   #redirect to user show page if the registration succeed 
-    def create
+  def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -22,8 +55,25 @@ class UsersController < ApplicationController
   
   private
 
-    def user_params
+  def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
+  end
+  
+  # Before filters
+  def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+        end
+  end
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,3 +8,4 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
+
